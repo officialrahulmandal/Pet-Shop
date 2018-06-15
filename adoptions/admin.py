8bb7d852a3c0ff8505da5df3d642dfda4d463f75
rahul <officialrahulmandal@gmail.com>
2018-06-15 21:06:24 +0530

    make a CMS editable admin interface

--- adoptions/admin.py
+++ adoptions/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from .models import Pet
 
-# Register your models here.
+from django.contrib import admin
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display=['name','species','breed', 'age', 'sex']
