17bd94fd32e2e5c329f8f23b1dcf0e63d9333007
rahul <officialrahulmandal@gmail.com>
2018-06-15 22:19:51 +0530

    homepage created and pet_detail view is created

--- adoptions/views.py
+++ adoptions/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<p> Welcome to the Homepage </p>')
+
+def pet_detail(request, id):
+    return HttpResponse("<p>showing the details of id {}</p>".format(id))
